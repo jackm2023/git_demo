07c5fdad1097381da92897d7b09f84915f8f0913
jackmaseeh <maseeh.personal@gmail.com>
2023-04-29 21:33:45 +0530

    changed file after reset

--- gittest.py
+++ gittest.py
@@ -1,1 +1,6 @@
 print ("this is my python first line")																																					
+ this is revert from master 
+this is change after git add from master
+this is second change after before git add
+
+after reset  good file
